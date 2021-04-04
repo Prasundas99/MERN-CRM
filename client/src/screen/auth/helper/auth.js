746f32e1e350895459e5b57feee232d9d57a4825
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-04-05 02:15:10 +0530

    add all user api helper

---
+++ client/src/screen/auth/helper/auth.js
@@ -0,0 +1,78 @@
+import axios from "axios";
+const API = process.env.REACT_APP_API;
+
+export const signup = async (user) => {
+  try {
+    const config = {
+      "Content-Type": "application/json",
+    };
+    const { data } = await axios.post(`${API}/user/signup`, user, config);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (user) => {
+  try {
+    const config = {
+      "Content-Type": "application/json",
+    };
+    const { data } = await axios.post(`${API}/user/login`, user, config);
+    console.log(data);
+    localStorage.setItem("userCreds", JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signout = () => {
+  if (typeof window !== "undefined") {
+    localStorage.removeItem("userCreds");
+  }
+};
+
+export const emailVerify = async (email) => {
+  try {
+    const config = {
+      "Content-Type": "application/json",
+    };
+    const { data } = await axios.post(
+      `${API}/user/email-verify`,
+      email,
+      config
+    );
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const resetPassword = async (modifiedPassword, accessToken) => {
+  try {
+    const config = {
+      "Content-Type": "application/json",
+    };
+    const { data } = await axios.post(
+      `${API}/user/reset-password/${accessToken}`,
+      modifiedPassword,
+      config
+    );
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const isAutheticated = () => {
+  if (typeof window == "undefined") {
+    return false;
+  }
+  if (localStorage.getItem("userCreds")) {
+    return JSON.parse(localStorage.getItem("userCreds"));
+  } else {
+    return false;
+  }
+};
