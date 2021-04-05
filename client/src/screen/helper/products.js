19577956e792cb856dd3c1a6168987aa7c4c4b1f
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-04-05 18:34:11 +0530

    add all API helper for product

--- client/src/screen/helper/products.js
+++ client/src/screen/helper/products.js
@@ -12,3 +12,36 @@ export const getProduct = async () => {
     throw new Error(`${err.message}`);
   }
 };
+
+export const createProduct = async () => {
+  try {
+    const { data } = await axios.post(`${API}/Product`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    const err = error && error.response && error.response.data;
+    throw new Error(`${err.message}`);
+  }
+};
+
+export const updateProduct = async (productId) => {
+  try {
+    const { data } = await axios.put(`${API}/Product/${productId}`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    const err = error && error.response && error.response.data;
+    throw new Error(`${err.message}`);
+  }
+};
+
+export const deleteProduct = async (productId) => {
+  try {
+    const { data } = await axios.delete(`${API}/Product/${productId}`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    const err = error && error.response && error.response.data;
+    throw new Error(`${err.message}`);
+  }
+};
