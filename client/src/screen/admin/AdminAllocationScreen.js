ab4286cf3beac84f7af7f6fe581981c825cad6f2
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-04-05 19:55:27 +0530

    connect customer product Allocation API with Admin Screen

--- client/src/screen/admin/AdminAllocationScreen.js
+++ client/src/screen/admin/AdminAllocationScreen.js
@@ -1,20 +1,34 @@
+import { useState, useEffect } from "react";
 //Components
-import Dashboard from '../../Components/admin/Admin'
-import AdminAllocation from '../../Components/admin/AdminAllocation';
-
+import Dashboard from "../../Components/admin/Admin";
+import AdminAllocation from "../../Components/admin/AdminAllocation";
 
 //Styles
-import { useStyles } from '../../styles/Homestyle';
+import { useStyles } from "../../styles/Homestyle";
 
+// API Connection
+import { getAllocation } from "../helper/allocation";
 
 function AdminAllocationScreen() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Dashboard />
-            <AdminAllocation />
-       </div>
-    )
+  const classes = useStyles();
+
+  const [allocation, setAllocation] = useState([]);
+
+  useEffect(() => {
+    getAllocation().then((data) => {
+      console.log(data);
+      setAllocation(data);
+    });
+  }, []);
+
+  console.log(allocation);
+
+  return (
+    <div className={classes.root}>
+      <Dashboard />
+      <AdminAllocation allocation={allocation} />
+    </div>
+  );
 }
 
 export default AdminAllocationScreen;
